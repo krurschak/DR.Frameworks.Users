620d2a69ed11b4ad6d525c789c97047ee27814b4
Krur Schak <krurschak@gmail.com>
2021-04-18 18:37:42 +0200

    edit user model and add event model for event stream

---
+++ src/DR.Frameworks.Users/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using DR.Packages.Mongo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DR.Frameworks.Users.Models
+{
+    public class Event : IIdentifiable
+    {
+        public Guid Id { get; set; }
+        public DateTime ConsumedAtUtc { get; set; }
+        public string MessageType { get; set; }
+        public string Message { get; set; }
+    }
+}
--- src/DR.Frameworks.Users/Models/User.cs
+++ src/DR.Frameworks.Users/Models/User.cs
@@ -7,6 +7,20 @@ namespace DR.Frameworks.Users.Models
 {
     public class User : IIdentifiable
     {
+        public User() { }
+
+        public User(Guid id, string identitySub, string identityIss, string email, int? salutation, string title, string lastName, string firstName)
+        {
+            Id = id;
+            IdentitySub = identitySub;
+            IdentityIss = identityIss;
+            Email = email;
+            Salutation = salutation ?? -9;
+            Title = title;
+            LastName = lastName;
+            FirstName = firstName;
+        }
+
         public Guid Id { get; set; }
         public string IdentitySub { get; set; }
         public string IdentityIss { get; set; }
