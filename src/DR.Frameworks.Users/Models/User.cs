2f9748df899328a33d01102e6d7c7db56b78baf6
Krur Schak <krurschak@gmail.com>
2021-04-18 10:17:19 +0200

    Initial code commit

---
+++ src/DR.Frameworks.Users/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using DR.Packages.Mongo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DR.Frameworks.Users.Models
+{
+    public class User : IIdentifiable
+    {
+        public Guid Id { get; set; }
+        public string IdentitySub { get; set; }
+        public string IdentityIss { get; set; }
+        public string Email { get; set; }
+        public int Salutation { get; set; }
+        public string Title { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
